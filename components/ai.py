2dcd5f01268df6a582223abf6af0c0a2cad17ff9
Nymall <pythagorus1987@gmail.com>
2020-05-13 19:37:11 -0700

    Monsters

--- components/ai.py
+++ components/ai.py
@@ -2,6 +2,17 @@ import libtcodpy as libtcod
 
 from random import randint
 
+from components.equipment import EquipmentSlots
+from components.equippable import Equippable
+from components.fighter import Fighter
+from components.item import Item
+from components.stairs import Stairs
+from components.inventory import Inventory
+
+from render_functions import RenderOrder
+
+from entity import Entity
+
 from game_messages import Message
 
 
@@ -13,6 +24,28 @@ class BasicMonster:
         if libtcod.map_is_in_fov(fov_map, monster.x, monster.y):
 
             if monster.distance_to(target) >= 2:
+                monster.move_astar(target, entities, game_map)
+
+            elif target.fighter.hp > 0:
+                attack_results = monster.fighter.attack(target)
+                results.extend(attack_results)
+
+        return results
+
+
+class BasicShaman:
+    def take_turn(self, target, fov_map, game_map, entities):
+        results = []
+
+        monster = self.owner
+        if libtcod.map_is_in_fov(fov_map, monster.x, monster.y):
+
+            if randint(1, 100) < 5:
+                message_log.add_message(Message('The shaman barks something, and a goblin materializes beside you!', libtcod.light_violet))
+                monster = Entity((randint(1,3) + target.x), (randint(1,3) + target.y), 'g', libtcod.green, 'Clanfear Goblin', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=15, defense=1, power=2, xp=45), ai=BasicMonster(), inventory=Inventory(5))
+                entities.append(monster)
+            
+            elif monster.distance_to(target) >= 2:
                 monster.move_astar(target, entities, game_map)
 
             elif target.fighter.hp > 0:
--- map_objects/summon_monster.py
+++ map_objects/summon_monster.py
@@ -1,7 +1,7 @@
 import libtcodpy as libtcod
-from random import randint
+import random
 
-from components.ai import BasicMonster
+from components.ai import BasicMonster, BasicShaman
 from components.equipment import EquipmentSlots
 from components.equippable import Equippable
 from components.fighter import Fighter
@@ -19,42 +19,88 @@ from random_utils import from_dungeon_level, random_choice_from_dict
 
 from render_functions import RenderOrder
 
-class monster_all(ENUM):
-    KOBOLD = 20,
-    GOBLIN = 20,
-    C_GOBLIN = 20,
-    CB_GOBLIN = 20,
-    CO_GOBLIN = 20,
-    CS_GOBLIN = 20,
-    ORC = 20,
-    TROLL = from_dungeon_level([[15, 3], [30, 5], [60, 7]], self.dungeon_level)
+from enum import *
 
-class monster_stats(monster):
-    selector = 1 #add all enum numbers
+class gen_monsters:
+    def monster_all(Enum):
+        KOBOLD = auto()
+        RAZORTOOTH = auto()
+        KHUNTER = auto()
+        KGLASSMITH = auto()
+        GOBLIN = auto()
+        C_GOBLIN = auto()
+        CB_GOBLIN = auto()
+        CO_GOBLIN = auto()
+        CS_GOBLIN = auto()
+        ORC = auto()
+        TROLL = auto()
 
-    if selector == KOBOLD:
-        monster = Entity(x, y, 'k', libtcod.desaturated_green, 'Kobold', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=10, defense=0, power=1, xp=35), ai=BasicMonster(), inventory=Inventory(5))
-    elif selector == GOBLIN:
-        monster = Entity(x, y, 'g', libtcod.green, 'Goblin', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=15, defense=0, power=2, xp=40), ai=BasicMonster(), inventory=Inventory(5))
-    elif selector == C_GOBLIN:
-        monster = Entity(x, y, 'g', libtcod.green, 'Clanfear Goblin', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=15, defense=1, power=2, xp=45), ai=BasicMonster(), inventory=Inventory(5))
-    elif selector == CB_GOBLIN:
-        monster = Entity(x, y, 'g', libtcod.green, 'Clanfear Goblin Brawler', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=15, defense=2, power=3, xp=60), ai=BasicMonster(), inventory=Inventory(5))
-        monster.inventory.add_item(Entity(0, 0, '-', libtcod.sky, 'Rusty Dagger', equippable=Equippable(EquipmentSlots.MAIN_HAND, power_bonus=2)))
-        monster.inventory.add_item(Entity(0, 0, '#', libtcod.brown, 'Ratty Peasant Shirt', equippable=Equippable(EquipmentSlots.CHEST, defense_bonus=1)))
-    elif selector == CO_GOBLIN:
-        monster = Entity(x, y, 'g', libtcod.green, 'Clanfear Goblin Organizer', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=25, defense=2, power=3, xp=80), ai=BasicMonster(), inventory=Inventory(5))
-        monster.inventory.add_item(Entity(0, 0, '-', libtcod.sky, 'Rusty Dagger', equippable=Equippable(EquipmentSlots.MAIN_HAND, power_bonus=2)))
-        monster.inventory.add_item(Entity(0, 0, '#', libtcod.brown, 'Ratty Peasant Shirt', equippable=Equippable(EquipmentSlots.CHEST, defense_bonus=1)))
-        monster.inventory.add_item(Entity(0, 0, '#', libtcod.brown, 'Ratty Cloth Toque', equippable=Equippable(EquipmentSlots.HELMET, max_hp_bonus=10)))
-        monster.inventory.add_item(Entity(0, 0, '#', libtcod.grey, 'Rusty Iron Ring', equippable=Equippable(EquipmentSlots.RING_R, max_blood_bonus=10)))
-    elif selector == CS_GOBLIN:
-        monster = Entity(x, y, 'g', libtcod.light_green, 'Clanfear Goblin Shaman', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=25, defense=2, power=3, xp=80), ai=BasicMonster(), inventory=Inventory(5))
-        monster.inventory.add_item(Entity(0, 0, '-', libtcod.sky, 'Staff of Impovrished Lightning', item=Item(use_function=cast_lightning_chance, damage=20, maximum_range=6)))
-        monster.inventory.add_item(Entity(0, 0, '#', libtcod.brown, 'Ratty Peasant Shirt', equippable=Equippable(EquipmentSlots.CHEST, defense_bonus=1)))
-        monster.inventory.add_item(Entity(0, 0, '#', libtcod.brown, 'Ratty Cloth Toque', equippable=Equippable(EquipmentSlots.HELMET, max_hp_bonus=10)))
-        monster.inventory.add_item(Entity(0, 0, '#', libtcod.grey, 'Rusty Iron Ring', equippable=Equippable(EquipmentSlots.RING_R, max_blood_bonus=10)))
-    elif selector == ORC:
-        monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=25, defense=1, power=2, xp=45), ai=BasicMonster(), inventory=Inventory(5))
-    elif selector == TROLL:
-        monster = Entity(x, y, 'T', libtcod.green, 'Troll', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=30, defense=2, power=6, xp=80), ai=BasicMonster(), inventory=Inventory(5))
+        def ret_mon():
+            mon = random.choice(list(monster_all))
+            return mon
+
+    def monster_goblin(Enum):
+        GOBLIN = auto()
+        C_GOBLIN = auto()
+        CB_GOBLIN = auto()
+        CO_GOBLIN = auto()
+        CS_GOBLIN = auto()
+    
+        def ret_mon():
+            mon = random.choice(list(monster_goblin))
+            return mon
+
+    def monster_kobold(Enum):
+        KOBOLD = auto()
+        RAZORTOOTH = auto()
+        KHUNTER = auto()
+        KGLASSMITH = auto()
+    
+        def ret_mon():
+            mon = random.choice(list(monster_kobold))
+            return mon
+
+    def monster_orc(Enum):
+        ORC = auto()
+        TROLL = auto()
+
+        def ret_mon():
+            mon = random.choice(list(monster_orc))
+            return mon
+
+    def monster_stats(x, y, sect=monster_all):
+        selector = sect.ret_mon()
+
+        if selector == monster_all.KOBOLD:
+            monster = Entity(x, y, 'k', libtcod.desaturated_green, 'Kobold', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=10, defense=0, power=1, xp=35), ai=BasicMonster(), inventory=Inventory(5))
+        if selector == monster_all.RAZORTOOTH:
+            monster = Entity(x, y, 'k', libtcod.desaturated_green, 'Kolbold Razortooth', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=20, defense=0, power=2, xp=45), ai=BasicMonster(), inventory=Inventory(5))
+        if selector == monster_all.KHUNTER:
+            monster = Entity(x, y, 'k', libtcod.desaturated_green, 'Kolbold Hunter', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=25, defense=1, power=3, xp=50), ai=BasicMonster(), inventory=Inventory(5))
+        if selector == monster_all.KGLASSMITH:
+            monster = Entity(x, y, 'k', libtcod.desaturated_green, 'Kolbold Glasssmith', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=30, defense=1, power=2, xp=50), ai=BasicMonster(), inventory=Inventory(5))
+            monster.inventory.add_item(Entity(0, 0, '/', libtcod.sky, 'Handfull of glass shards', equippable=Equippable(EquipmentSlots.MAIN_HAND, power_bonus=2)))
+        elif selector == monster_all.GOBLIN:
+            monster = Entity(x, y, 'g', libtcod.green, 'Goblin', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=15, defense=0, power=2, xp=40), ai=BasicMonster(), inventory=Inventory(5))
+        elif selector == monster_all.C_GOBLIN:
+            monster = Entity(x, y, 'g', libtcod.green, 'Clanfear Goblin', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=15, defense=1, power=2, xp=45), ai=BasicMonster(), inventory=Inventory(5))
+        elif selector == monster_all.CB_GOBLIN:
+            monster = Entity(x, y, 'g', libtcod.green, 'Clanfear Goblin Brawler', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=15, defense=2, power=3, xp=60), ai=BasicMonster(), inventory=Inventory(5))
+            monster.inventory.add_item(Entity(0, 0, '-', libtcod.sky, 'Rusty Dagger', equippable=Equippable(EquipmentSlots.MAIN_HAND, power_bonus=2)))
+            monster.inventory.add_item(Entity(0, 0, '#', libtcod.brown, 'Ratty Peasant Shirt', equippable=Equippable(EquipmentSlots.CHEST, defense_bonus=1)))
+        elif selector == monster_all.CO_GOBLIN:
+            monster = Entity(x, y, 'g', libtcod.green, 'Clanfear Goblin Organizer', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=25, defense=2, power=3, xp=80), ai=BasicMonster(), inventory=Inventory(5))
+            monster.inventory.add_item(Entity(0, 0, '-', libtcod.sky, 'Rusty Dagger', equippable=Equippable(EquipmentSlots.MAIN_HAND, power_bonus=2)))
+            monster.inventory.add_item(Entity(0, 0, '#', libtcod.brown, 'Ratty Peasant Shirt', equippable=Equippable(EquipmentSlots.CHEST, defense_bonus=1)))
+            monster.inventory.add_item(Entity(0, 0, '#', libtcod.brown, 'Ratty Cloth Toque', equippable=Equippable(EquipmentSlots.HELMET, max_hp_bonus=10)))
+            monster.inventory.add_item(Entity(0, 0, '#', libtcod.grey, 'Rusty Iron Ring', equippable=Equippable(EquipmentSlots.RING_R, max_blood_bonus=10)))
+        elif selector == monster_all.CS_GOBLIN:
+            monster = Entity(x, y, 'g', libtcod.light_green, 'Clanfear Goblin Shaman', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=45, defense=2, power=3, xp=100), ai=BasicShaman(), inventory=Inventory(5))
+            monster.inventory.add_item(Entity(0, 0, '-', libtcod.sky, 'Staff of Impovrished Lightning', item=Item(use_function=cast_lightning_chance, damage=20, maximum_range=6)))
+            monster.inventory.add_item(Entity(0, 0, '#', libtcod.brown, 'Ratty Peasant Shirt', equippable=Equippable(EquipmentSlots.CHEST, defense_bonus=1)))
+            monster.inventory.add_item(Entity(0, 0, '#', libtcod.brown, 'Ratty Cloth Toque', equippable=Equippable(EquipmentSlots.HELMET, max_hp_bonus=10)))
+            monster.inventory.add_item(Entity(0, 0, '#', libtcod.grey, 'Rusty Iron Ring', equippable=Equippable(EquipmentSlots.RING_R, max_blood_bonus=10)))
+        elif selector == monster_all.ORC:
+            monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=25, defense=1, power=2, xp=45), ai=BasicMonster(), inventory=Inventory(5))
+        elif selector == monster_all.TROLL:
+            monster = Entity(x, y, 'T', libtcod.green, 'Troll', blocks=True, render_order=RenderOrder.ACTOR, fighter=Fighter(hp=30, defense=2, power=6, xp=80), ai=BasicMonster(), inventory=Inventory(5))
