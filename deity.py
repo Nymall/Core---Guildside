9772423ab93d76a9f098b45c2b7fbcf2234a812d
Nymall <pythagorus1987@gmail.com>
2020-07-14 13:47:35 -0700

    Adding initial XML support

--- deity.py
+++ deity.py
@@ -1,4 +1,13 @@
 from random import randint
+import xml.dom.minidom
+
+from components.ai import BasicMonster
+from components.equipment import EquipmentSlots
+from components.equippable import Equippable
+from components.fighter import Fighter
+from components.item import Item
+
+from entity import Entity
 
 class deity:
     """This is the thing your programming teacher told you never to do.... a god class."""
@@ -27,6 +36,20 @@ class deity:
         # https://www.guru99.com/manipulating-xml-with-python.html
         # Browse through the item list, and enumerate items by cycling through all xml files
         print()
+
+    def readlist(self, file):
+        doc = xml.dom.minidom.parse(file)
+        items = doc.getElementsByTagName("item")
+        for ind_items in items:
+            name = ind_items.getElementsByTagName("name")
+            if self.itemlist[name] == name:
+                #do nothing
+                print()
+            else:
+                # item_component = Item(use_function=heal, amount=20)
+                # item = Entity(x, y, '!', libtcod.violet, 'Lesser Healing Potion', render_order=RenderOrder.ITEM, item=item_component)
+                self.itemlist[name] = Entity(0, 0, ind_items.getElementsByTagName("symbol"), ind_items.getElementsByTagName("color"), ind_items.getElementsByTagName("name"), ind_items.getElementsByTagName("rorder"), item=Item(ind_items.getElementsByTagName("use")))
+
 
     def generate_monster_lists(self):
         print()
